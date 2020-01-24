f9451e0f8dd020f3a0020447f567086647cfcc94
Jorgel48 <jorge.camacho@my.tccd.edu>
2020-01-24 19:40:40 +0000

    Everything

--- main.cpp
+++ main.cpp
@@ -1,5 +1,38 @@
+/*
+name: Jorge Camacho
+Course Section: COSC-1436-57002
+Editor: repl.it
+Summary:
+*/
 #include <iostream>
+using namespace std;
 
 int main() {
-  std::cout << "Hello World!\n";
-}
+  //Input
+  int i;
+  cout << "How many pennis do you have?: ";
+  cin >> i;
+  //Process 
+  int dollars = 100;
+  int quarters = 25;
+  int dimes = 10;
+  int nickels = 5;
+  int d = i / dollars;
+  i %= dollars;
+  int q = i / quarters;
+  i %= quarters;
+  int di = i / dimes;
+  i %= dimes;
+  int n = i / nickels;
+  i %= nickels;
+  //Output
+  cout << "dollars " << d << endl;
+  cout << "quarters " << q << endl;
+  cout << " dimes " << di << endl;
+  cout << " nickels " << n << endl;
+  cout << "pennis" << i << endl;
+
+
+
+  return 0;
+} 
